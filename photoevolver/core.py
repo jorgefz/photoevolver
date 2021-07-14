d20f98058e8f6dd8d484811eff7ccc30f4ce8a9c
Jorge Fernandez <jorgefz.fernandez@gmail.com>
2021-07-14 15:16:00 +0100

    1.2 - Added Otegi M-R relations & evolve back

--- photoevolver/core.py
+++ photoevolver/core.py
@@ -7,8 +7,199 @@
 from copy import deepcopy
 import numpy as np
 import astropy.constants as Const
+import Mors as mors
 
-from .EvapMass.planet_structure import mass_to_radius_solid as OwenMassRadiusRelation
+from .EvapMass.planet_structure import mass_to_radius_solid as owen_mr_func
+
+
+# Wrapper for EvapMass M-R relation
+def OwenMassRadiusRelation(**kwargs):
+    # Requires: mass=mcore, Xiron, Xice
+    if 'mass'  not in kwargs.keys(): return None
+    if 'Xice'  not in kwargs.keys(): kwargs['Xice']  = 0.0
+    if 'Xiron' not in kwargs.keys(): kwargs['Xiron'] = 1/3
+    return owen_mr_func(mass=kwargs['mass'], Xiron=kwargs['Xiron'], Xice=kwargs['Xice'])
+
+def OtegiMassRadiusRelation(**kwargs):
+    """
+    Computes radius based on planets mass following the mass-radius
+    relationships by Otegi et al. (2015).
+    https://ui.adsabs.harvard.edu/abs/2020A%26A...634A..43O/abstract
+    Input:
+            mass 	:(float) Mass in Earth masses
+            otegi_error :(float) Uncertainty of the mass
+            otegi_mode 	:(str) "rocky", "volatile"
+    Returns:
+            [0] 	:(float) Radius in Earth radii
+            [1] 	:(float) Error in radius, if mass error provided
+    """
+    # Requires: mass, error, mode
+    mass = kwargs['mass']
+    error = kwargs['otegi_error'] if 'error' in kwargs.keys() else 0.0
+    mode = kwargs['otegi_mode'] if 'mode' in kwargs.keys() else 'rocky'
+    otegi_models = [
+        dict(mode='rocky', const=1.03, c_err=0.02, pow=0.29, p_err=0.01),
+        dict(mode='volatile', const=0.7, c_err=0.11, pow=0.63, p_err=0.04)
+    ]
+
+    if(mode not in ['rocky','volatile']): print(f"Error: unknown mode '{mode}'")
+    model = otegi_models[0] if mode=='rocky' else otegi_models[1]
+    radius = model['const'] * mass ** model['pow']
+    # Errors
+    const_term = model['c_err'] * mass ** model['pow']
+    mass_term =  model['const'] * model['pow'] * error * mass ** (model['pow']-1)
+    pow_term =   model['const'] * mass ** model['pow'] * np.log(mass) * model['p_err']
+    radius_err = np.sqrt( (const_term)**2 + (mass_term)**2 + (pow_term)**2 )
+    
+    if error == 0.0: return radius
+    return radius, radius_err
+
+
+def evolve_forward(planet, mloss, struct, star, time_step=1.0, age_end=1e4, **kwargs):
+    """
+    Evolves a planet forward in time.
+    Parameters:
+        planet:     (object) photoevolve.core.Planet object previously initialised
+        mloss:      (class) Mass loss formulation from photoevolve.massloss
+        struct:     (class) Function that relates the envelope radius to the envelope mass fraction
+        star:       (object OR dict) Description of X-ray evolution of the host star. Three options:
+                        > Mors.Star object
+                        > Dict of two arrays: 'Lxuv' and 'Lbol' luminosity tracks of length (age_end - planet.age + 1)/time_step
+        time_step:  (float) Time step of the simulation in Myr
+        age_end:    (float) Oldest age to which to run simulation
+        **kwargs:   Specific parameters to tailor your simulation:
+                        'eff': mass loss efficiency if using the EnergyLimited formulation.
+                        'beta': Rxuv / Rp
+                        'mstar': Mass of the host star in M_sun.
+    """
+    pl = deepcopy(planet)
+    if type(pl) != Planet:
+        raise ValueError("the planet must be an instance of the photoevolve.core.Planet class")
+    if not callable(struct):
+        raise ValueError("struct must be a function")
+    if age_end <= pl.age:
+        raise ValueError("The maximum age must be greater than the planet's age")
+    if type(star) != mors.Star:
+        raise NotImplementedError("The star must be a Mors.Star instance")
+
+    tracks = _init_tracks()
+    params = kwargs
+
+    # Initial definition of Renv and Rp
+    params = Evolve.update_params(params, pl, star)
+    pl.renv = struct(**params)
+    pl.rp = pl.rcore + pl.renv
+    # Update input planet unknowns
+    planet.renv = pl.renv
+    planet.rp = pl.rp
+
+    while(pl.age < age_end):
+        # Parameters
+        params = Evolve.update_params(params, pl, star)
+        # Evolution
+        # - mass loss
+        if mloss is not None and pl.fenv > 1e-4:
+            pl.menv -= mloss(**params)
+            pl.mp = pl.mcore + pl.menv 
+            pl.fenv = pl.menv / pl.mp
+            params = _update_params(params, pl, star)
+        # - envelope radius
+        pl.renv = struct(**params)
+        pl.rp = pl.rcore + pl.renv
+        # Update Tracks
+        tracks = _update_tracks(tracks, pl, star)
+        # Jump
+        pl.age += time_step
+    return tracks
+
+
+def evolve_back(planet, mloss, struct, star, time_step=1.0, age_end=1.0, **kwargs):
+    """
+    Evolves a planet backwards in time.
+    Parameters:
+        planet:     (object) photoevolve.core.Planet object previously initialised
+        mloss:      (class) Mass loss formulation from photoevolve.massloss
+        struct:     (class) Function that relates the envelope radius to the envelope mass fraction
+        star:       (object OR dict) Description of X-ray evolution of the host star. Three options:
+                        > Mors.Star object
+                        > Dict of two arrays: 'Lxuv' and 'Lbol' luminosity tracks of length (age_end - planet.age + 1)/time_step
+        time_step:  (float) Time step of the simulation in Myr
+        age_end:    (float) Earliest age to which to run simulation
+        **kwargs:   Specific parameters to tailor your simulation:
+                        'eff': mass loss efficiency if using the EnergyLimited formulation.
+                        'beta': Rxuv / Rp
+                        'mstar': Mass of the host star in M_sun.
+    """
+    pl = deepcopy(planet)
+    if type(pl) != Planet:
+        raise ValueError("the planet must be an instance of the photoevolve.core.Planet class")
+    if not callable(struct):
+        raise ValueError("struct must be a function")
+    if age_end >= pl.age:
+        raise ValueError("The miminum age must be lower than the planet's age")
+    if type(star) != mors.Star:
+        raise NotImplementedError("The star must be a Mors.Star instance")
+
+    tracks = _init_tracks()
+    params = kwargs
+
+    # Initial definition of Renv and Rp
+    params = Evolve.update_params(params, pl, star)
+    pl.renv = struct(**params)
+    pl.rp = pl.rcore + pl.renv
+    # Update input planet unknowns
+    planet.renv = pl.renv
+    planet.rp = pl.rp
+    if pl.fenv < 1e-4: pl.fenv = 1e-4
+
+    while(pl.age > age_end):
+        # Parameters
+        params = Evolve.update_params(params, pl, star)
+        # Evolution
+        # - mass loss
+        if mloss is not None and pl.fenv > 1e-4:
+            pl.menv += mloss(**params)
+            pl.mp = pl.mcore + pl.menv 
+            pl.fenv = pl.menv / pl.mp
+            params = _update_params(params, pl, star)
+        # - envelope radius
+        pl.renv = struct(**params)
+        pl.rp = pl.rcore + pl.renv
+        # Update Tracks
+        tracks = _update_tracks(tracks, pl, star)
+        # Jump
+        pl.age -= time_step
+    tracks = _reverse_tracks(tracks)
+    return tracks
+
+
+def _init_tracks():
+    fields = ('Age', 'Lbol', 'Lx', 'Rp', 'Mp', 'Menv', 'Renv', 'Fenv', 'Mloss', 'Dens')
+    tracks = dict( Age=[], Lbol=[], Rp=[], Mp=[] )
+    return tracks
+
+def _update_tracks(tracks, planet, star):
+    tracks['Age'].append(planet.age)
+    tracks['Lbol'].append(star.Lbol(planet.age))
+    tracks['Rp'].append(planet.rp)
+    tracks['Mp'].append(planet.mp)
+    return tracks
+
+def _reverse_tracks(tracks):
+    for k in tracks.keys():
+        tracks[k].reverse()
+    return tracks
+
+def _update_params(params, planet, star):
+    fbol = star.Lbol(planet.age) / (planet.dist*Const.au.to('cm').value)**2 / (4*np.pi)
+    params.update(planet.__dict__)
+    params['fbol'] = fbol
+    params['mass'] = planet.mp
+    params['radius'] = planet.rp
+    params['Lxuv'] = star.Lx(planet.age) + star.Leuv(planet.age)
+    params['mstar'] = star.Mstar
+    return params
+
 
 class Evolve:
     def forward(planet, mloss, struct, star, time_step=1.0, age_end=1e4, **kwargs):
@@ -19,9 +210,8 @@ class Evolve:
             mloss:      (class) Mass loss formulation from photoevolve.massloss
             struct:     (class) Function that relates the envelope radius to the envelope mass fraction
             star:       (object OR dict) Description of X-ray evolution of the host star. Three options:
-                            Mors.Star object
-                            Dictionary with keys 'Rx_sat' for the saturation activity, 't_sat' in Myr, and 'power' for the power-law index.
-                            Tuple of two arrays: [Lxuv, Lbol] luminosities
+                            > Mors.Star object
+                            > Dict of two arrays: 'Lxuv' and 'Lbol' luminosity tracks of length (age_end - planet.age + 1)/time_step
             time_step:  (float) Time step of the simulation in Myr
             age_end:    (float) Oldest age to which to run simulation
             **kwargs:   Specific parameters to tailor your simulation:
@@ -32,12 +222,14 @@ class Evolve:
         pl = deepcopy(planet)
         print(type(pl))
         if type(pl) != Planet:
-            raise ValueError("the pl must be an instance of the photoevolve.core.Planet class")
+            raise ValueError("the planet must be an instance of the photoevolve.core.Planet class")
         if not callable(struct):
             raise ValueError("struct must be a function")
         if age_end <= pl.age:
-            raise ValueError("The miminum and maximum ages mist me lower/greater than the defined pl age")
-    
+            raise ValueError("The miminum and maximum ages must be lower/greater than the defined pl age")
+        if type(star) != mors.Star:
+            raise NotImplementedError("The star must be a Mors.Star instance")
+
         tracks = dict( Age=[], Lbol=[], Rp=[], Mp=[] )
         params = kwargs
 
@@ -87,7 +279,7 @@ class Evolve:
 
 
 class Planet:
-    def __init__(self, mp=None, rp=None, mcore=None, rcore=None, menv=None, renv=None, fenv=None, dist=None, age=None, comp=None):
+    def __init__(self, mp=None, rp=None, mcore=None, rcore=None, menv=None, renv=None, fenv=None, dist=None, age=None, comp=None, mr=None):
         """
         Parameters:
             mp:     Planet mass (M_earth)
@@ -100,6 +292,7 @@ class Planet:
             dist:   Separation between the planet and the star (AU)
             age:    Age of the planet (Myr)
             comp:   Dictionary with keys [Xice, Xrock, Xiron] (mass abundances)
+            mr:     Mass-Radius relation to use to calculate core radius
         """
         self.comp = comp
         self.mp = mp
@@ -111,12 +304,16 @@ class Planet:
         self.fenv = fenv
         self.dist = dist
         self.age = age
+        self.mr = mr
         # --
         self.input_check()
 
     def input_check(self):
         if self.dist is None or self.age is None:
             raise ValueError("age or orbital distance undefined")
+
+        if self.mr is None or not callable(self.mr):
+            self.mr = OwenMassRadiusRelation
 
         if type(self.comp) != dict or \
            (type(self.comp) == dict and tuple(self.comp.keys()) != ['Xiron','Xice']) or \
@@ -127,8 +324,8 @@ class Planet:
             raise ValueError("core mass undefined")
         if self.rcore is None:
             print(f" Warning: core radius undefined.", end='')
-            print(f" Will be estimated from its composition (Iron:{self.comp['Xiron']:.2f}, Ice:{self.comp['Xice']:.2f})...")
-            self.rcore = OwenMassRadiusRelation(mass=self.mcore, Xiron=self.comp['Xiron'], Xice=self.comp['Xice'])
+            print(f" Will be estimated from a mass-radius relation ", end='')
+            self.rcore = self.mr(mass=self.mcore, Xiron=self.comp['Xiron'], Xice=self.comp['Xice'])
             print(f" -> {self.rcore:.3f} Earth radii")
 
         # Temporal solution until solving for fenv is implemented
--- photoevolver/version.py
+++ photoevolver/version.py
@@ -1,2 +1,25 @@
-__version__ = "1.1"
+__version__ = "1.2"
 __author__ = "Jorge Fernandez"
+
+"""
+
+########################
+#     PHOTOEVOLVER     #
+########################
+
+--- Changelog ---
+
+Version 1.2
+    - Added option to calculate planet core radius from custom mass-radius relation.
+    - Added Otegi et al (2020) mass-radius relations.
+    - Changed class ph.core.Evolve.forward to function ph.core.evolve_forward
+    - Added backwards evolution with evolve_back.
+    - Updated README
+
+Version 1.1
+    - Fixed ph.core.Planet repr when some variables are None
+
+Version 1.0
+    - First working version
+
+"""
