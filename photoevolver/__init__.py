d5576fffdedc8122ef99084d562674c239b377c3
jorgefz <jorgefz.fernandez@gmail.com>
2023-02-02 09:50:36 +0000

    Switched to toml packaging config

--- photoevolver/__init__.py
+++ photoevolver/__init__.py
@@ -8,7 +8,7 @@ Package description here
 
 """
 
-from .version import __version__,__author__
+from .version import __version__, __author__
 import os
 import platform
 
--- setup.py
+++ setup.py
@@ -1,52 +1,52 @@
 import setuptools
-from photoevolver.version import __version__
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 ext_path = "photoevolver/cmodels/src"
-extensions = [
-    setuptools.Extension(
-        "photoevolver.cmodels",
-        [
-            ext_path+"/mloss.c",
-            ext_path+"/struct.c",
-            ext_path+"/cmodels.c",
-        ],
-        depends = [
-            ext_path+"/constants.h",
-            ext_path+"/models.h",
-        ],
-        include_dirs = [ext_path]   
-    )
-]
-
-setuptools.setup(
-    name="photoevolver",
-    version=__version__,
-    author="Jorge Fernandez",
-    author_email="Jorge.Fernandez-Fernandez@warwick.ac.uk",
-    
-    package_dir={"": "."},
-    packages=setuptools.find_packages(where="."),
-    ext_modules = extensions,
-
-    description="Planetary evolver written in Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jorgefz/photoevolver",
-    license="LICENSE",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
-        "Operating System :: OS Independent",
+ext_cmodels = setuptools.Extension(
+    "photoevolver.cmodels",
+    [
+        ext_path+"/mloss.c",
+        ext_path+"/struct.c",
+        ext_path+"/cmodels.c",
     ],
-    python_requires=">=3.6",
-    install_requires=[
-       "astropy",
-       "numpy",
-       "scipy",
-       "matplotlib"
-#       "Mors"
-   ],
+    depends = [
+        ext_path+"/constants.h",
+        ext_path+"/models.h",
+    ],
+    include_dirs = [ext_path]   
 )
+
+setuptools.setup(ext_modules=[ext_cmodels])
+
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+# setuptools.setup(
+    # name="photoevolver",
+    # version=__version__,
+    # author="Jorge Fernandez",
+    # author_email="Jorge.Fernandez-Fernandez@warwick.ac.uk",
+    
+    # package_dir={"": "."},
+    # packages=setuptools.find_packages(where="."),
+    # ext_modules = extensions,
+
+    # description="Planetary evolver written in Python",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/jorgefz/photoevolver",
+    # license="LICENSE",
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: GPL License",
+    #     "Operating System :: OS Independent",
+    # ],
+    # python_requires=">=3.6",
+    # install_requires=[
+    #    "astropy",
+    #    "numpy",
+    #    "scipy",
+    #    "matplotlib",
+    #    "uncertainties"
+    # ],
+# )
