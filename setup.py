4bcb6148021dfab57f05c5a87c420c14be7757c9
jorgefz <jorgefz.fernandez@gmail.com>
2023-02-02 09:51:06 +0000

    Cleaned up setup file

--- setup.py
+++ setup.py
@@ -17,36 +17,3 @@ ext_cmodels = setuptools.Extension(
 
 setuptools.setup(ext_modules=[ext_cmodels])
 
-
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
-
-# setuptools.setup(
-    # name="photoevolver",
-    # version=__version__,
-    # author="Jorge Fernandez",
-    # author_email="Jorge.Fernandez-Fernandez@warwick.ac.uk",
-    
-    # package_dir={"": "."},
-    # packages=setuptools.find_packages(where="."),
-    # ext_modules = extensions,
-
-    # description="Planetary evolver written in Python",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    # url="https://github.com/jorgefz/photoevolver",
-    # license="LICENSE",
-    # classifiers=[
-    #     "Programming Language :: Python :: 3",
-    #     "License :: OSI Approved :: GPL License",
-    #     "Operating System :: OS Independent",
-    # ],
-    # python_requires=">=3.6",
-    # install_requires=[
-    #    "astropy",
-    #    "numpy",
-    #    "scipy",
-    #    "matplotlib",
-    #    "uncertainties"
-    # ],
-# )
