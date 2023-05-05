0ad9227dd3643103f989de79a81281691db713d8
jorgefz <jorgefz.fernandez@gmail.com>
2023-05-05 11:09:05 +0100

    Updated models tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,15 +8,14 @@ from photoevolver.evostate import EvoState
 
 def test_core_otegi20():
     
-    valid_state = EvoState(mcore = 1.0)
-    result = models.core_otegi20(valid_state, dict())
+    result = models.core_otegi20(mcore = 1.0)
     assert np.isclose(result, 1.0, rtol=0.03)
 
-    bad_states = EvoState(), EvoState(mcore=np.nan), EvoState(mcore=-1)
+    bad_states = {}, {'mcore':np.nan}, {'mcore':-1}
     for b in bad_states:
-        with pytest.raises(ValueError):
-            models.core_otegi20(b, dict())
+        with pytest.raises((ValueError, TypeError)):
+            models.core_otegi20(**b)
     
-    uncert_state = EvoState(mcore = uncert.ufloat(1.0,0.1))
-    result_err   = models.core_otegi20(uncert_state, dict(ot20_errors=True))
+    uncert_state = {'mcore': uncert.ufloat(1.0,0.1), 'ot20_errors':True}
+    result_err   = models.core_otegi20(**uncert_state)
     assert result == result_err.nominal_value
