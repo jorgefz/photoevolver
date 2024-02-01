0ae5e5ea7b112c35e1cba45ec55d270bf88d5e10
jorgefz <jorgefz.fernandez@gmail.com>
2024-02-01 13:55:09 +0000

    Added Zeng+19 models for core with water content

--- photoevolver/models.py
+++ photoevolver/models.py
@@ -6,10 +6,123 @@ import sys
 import os
 import contextlib
 import numpy as np
+from scipy.interpolate import interp1d, RegularGridInterpolator
 import uncertainties as uncert
 from uncertainties import umath
 from . import physics, utils
 from . import cmodels
+
+
+__model_data_dir = os.path.dirname(os.path.realpath(__file__)) + "/model_data/"
+
+__model_data_cache_zeng19 :dict[interp1d|None] = {
+    'rock'       : None, # 100% MgSiO2
+    'earth'      : None, # 33% Fe + 67% MgSiO2 (Earth-like)
+    'iron'       : None, # 100% Fe
+    'water'      : None, # 100% H2O
+    'water_rock' : None  # 50% H2O + 50% Earth-like
+}
+
+
+def core_zeng19_rock(mcore: float, **kwargs) -> float:
+    """ Calculates planet radius from its mass assuming a pure rock (100% MgSiO2) composition using the model by Zeng+19 """
+    if __model_data_cache_zeng19['rock'] is None:
+        data = np.loadtxt(__model_data_dir + 'zeng19/core_zeng19_rock.csv', delimiter = ',')
+        masses, radii = data.T
+        __model_data_cache_zeng19['rock'] = interp1d(x = masses, y = radii,
+            kind = 'cubic', fill_value="extrapolate", assume_sorted = True)
+
+    model = __model_data_cache_zeng19['rock']
+    return float(model(mcore))
+
+
+def core_zeng19_iron(mcore: float, **kwargs) -> float:
+    """ Calculates planet radius from its mass assuming a pure iron (100% Fe) composition using the model by Zeng+19 """
+    if __model_data_cache_zeng19['iron'] is None:
+        data = np.loadtxt(__model_data_dir + 'zeng19/core_zeng19_iron.csv', delimiter = ',')
+        masses, radii = data.T
+        __model_data_cache_zeng19['iron'] = interp1d(x = masses, y = radii,
+            kind = 'cubic', fill_value="extrapolate", assume_sorted = True)
+
+    model = __model_data_cache_zeng19['iron']
+    return float(model(mcore))
+
+
+def core_zeng19_earth_like(mcore: float, **kwargs) -> float:
+    """ Calculates planet radius from its mass assuming an Earth-like (1/3 iron, 2/3 rock) composition using the model by Zeng+19 """
+    if __model_data_cache_zeng19['earth'] is None:
+        data = np.loadtxt(__model_data_dir + 'zeng19/core_zeng19_earth.csv', delimiter = ',')
+        masses, radii = data.T
+        __model_data_cache_zeng19['earth'] = interp1d(x = masses, y = radii,
+            kind = 'cubic', fill_value="extrapolate", assume_sorted = True)
+
+    model = __model_data_cache_zeng19['earth']
+    return float(model(mcore))
+
+
+def core_zeng19_water(mcore :float, lbol :float, sep :float, albedo :float = 0.2, **kwargs):
+    """ Calculates planet radius from its mass assuming a pure water (100% H2O) composition using the model by Zeng+19 """
+    fbol = physics.get_flux(lbol, dist_au = sep)
+    teq = physics.temp_eq(fbol)
+    
+    # Load cache of model sequences interpolating equilibrium temperature
+    if __model_data_cache_zeng19['water'] is None:
+        # Load model sequences
+        temp = 300, 500, 700, 1000
+        sequences = [ np.loadtxt(__model_data_dir + f"zeng19/core_zeng19_water_{t}K.csv", delimiter = ',') for t in temp ]
+        models    = [
+            interp1d(x = seq.T[0], y = seq.T[1],
+                kind = 'cubic', fill_value="extrapolate", assume_sorted = True)
+            for seq in sequences
+        ]
+        # Resample into a regular grid
+        regular_masses = np.logspace(np.log10(0.05), np.log10(500), base = 10, num = 50)
+        regular_radii  = [ model(regular_masses) for model in models ]
+
+        # 2D interpolation
+        __model_data_cache_zeng19['water'] = RegularGridInterpolator(
+            points = (temp, regular_masses),
+            values = regular_radii,
+            method = 'slinear',
+            bounds_error = False,
+            fill_value = None # extrapolate
+        )
+
+    model = __model_data_cache_zeng19['water']
+    return float(model([teq, mcore]))
+
+
+def core_zeng19_water_rock(mcore :float, lbol :float, sep :float, albedo :float = 0.2, **kwargs):
+    """ Calculates planet radius from its mass assuming a water + rock (50% H2O+ 50% Earth-like) composition using the model by Zeng+19 """
+    fbol = physics.get_flux(lbol, dist_au = sep)
+    teq = physics.temp_eq(fbol)
+    
+    # Load cache of model sequences interpolating equilibrium temperature
+    if __model_data_cache_zeng19['water_rock'] is None:
+        # Load model sequences
+        temp = 300, 500, 700, 1000
+        sequences = [ np.loadtxt(__model_data_dir + f"zeng19/core_zeng19_water_rock_{t}K.csv", delimiter = ',') for t in temp ]
+        models    = [
+            interp1d(x = seq.T[0], y = seq.T[1],
+                kind = 'cubic', fill_value="extrapolate", assume_sorted = True)
+            for seq in sequences
+        ]
+        # Resample into a regular grid
+        regular_masses = np.logspace(np.log10(0.5), np.log10(64), base = 10, num = 50)
+        regular_radii  = [ model(regular_masses) for model in models ]
+
+        # 2D interpolation
+        __model_data_cache_zeng19['water_rock'] = RegularGridInterpolator(
+            points = (temp, regular_masses),
+            values = regular_radii,
+            method = 'slinear',
+            bounds_error = False,
+            fill_value = None # extrapolate
+        )
+
+    model = __model_data_cache_zeng19['water_rock']
+    return float(model([teq, mcore]))
+
 
 def core_otegi20(
         mcore :float,
